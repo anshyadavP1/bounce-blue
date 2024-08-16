b0ba7446f17dd070ddcd79d965d074de75deb614
anshyadavP1 <ansh.yadav.sod23@anu.edu.in>
2024-08-17 00:08:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+// Reserve memory for x and y
+let x, y, step;
+
+function setup() {
+  createCanvas(400, 400);
+  // Place x and y in the middle of screen
+  x = width / 2;
+  y = height / 2;
+  
+  step = 5;
+  
+  // "dip a brush" into color once
+  // the color will remain the same unless I change it again
+  fill("blue");
+}
+
+function draw() {
+  // Clear the background
+  background(0);
+  // Draw a circle
+  circle(x, y, 100);
+ // Increase the x variable by step on each new frame
+  x += step;
+
+  // Flip the step after circle moves off the right OR left side
+  if (x >= width || x <= 0) {
+    step = -step;
+  }
+  
+  // TODO: repeat the same for vertical movement
+  // MORE TODO: randomize the increment
+}
